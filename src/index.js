80c84a02f26942cbeaaf32a3ff11340fcc9cbfa2
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-02-24 12:01:25 +0330

    add Provider and store to index.js for creating redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './redux/reducer'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './redux/Reducer'
 
-const store = createStore(rootReducer) ;
+const store = createStore(rootReducer)
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
